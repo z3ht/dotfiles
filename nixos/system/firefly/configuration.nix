31f40bfd8bbbd3bbfc500ac36aab39ca7a734e4b
Andrew Darling <andr3w.darling@gmail.com>
2022-12-04 18:09:15 -0600

    Update configuration.nix

--- nixos/system/firefly/configuration.nix
+++ nixos/system/firefly/configuration.nix
@@ -7,6 +7,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.initrd.kernelModules = [ "i915" ];
 
   networking.hostName = "firefly";
   networking.networkmanager.enable = true;
